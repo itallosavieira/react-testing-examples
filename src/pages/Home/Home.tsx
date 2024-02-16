a6b41968dd70d45ddeb8c2dc59b477349c853ae5
itallo.vieira.glo <itallo.sa.vieira@gmail.com>
2024-02-16 19:15:39 -0300

    feat: add Home unit tests

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
+
 const Home = () => {
+  const [buttonText, setButtonText] = useState('Show');
+
+  const handleClick = () => {
+    setButtonText((prev) => (
+      prev === 'Show' ? 'Hide' : 'Show'
+    ))
+  }
   return (
-    <h2> Home </h2>
+    <>
+      <h2> Home </h2>
+      <button onClick={handleClick} role="button"> {buttonText} </button>
+      { 
+        buttonText === 'Show' ? 
+        '' :
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+      }
+    </>
   )
 }
 
